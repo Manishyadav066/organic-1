c6fcafc79f06b23274882d99d7d625d68209f1cd
Your Name <you@example.com>
2026-03-22 17:26:49 +0530

    Update DB credentials for InfinityFree

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "ecommerce";
+$host = "sql303.infinityfree.com";
+$user = "if0_41449450";
+$pass = "Manish1273";
+$dbname = "if0_41449450_XXX"; // Update 'XXX' to the actual database name if you masked it
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
